c36ee615f4b8da81fec316a19e3454d4da4ad145
David KOMAR <dkomar@expedia.com>
2017-10-06 13:57:09 +0200

    test

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.Loader;
 using System.Threading.Tasks;
@@ -22,6 +23,7 @@ namespace ConsoleApp1
 			}
 			else
 			{
+				// var loader = new RuntimeAssemblyLoader(@"..\output");
 				var loader = new RuntimeAssemblyLoader(@"..\output");
 				pluginAssembly = loader.LoadFromAssemblyPath(new FileInfo(@"..\output\ClassLibrary1.Implementation.dll").FullName);
 			}
@@ -37,6 +39,29 @@ namespace ConsoleApp1
 			Console.WriteLine();
 			Console.WriteLine("Completed... [Press enter to terminate]");
 			Console.ReadLine();
+		}
+	}
+
+	public class AssemblyLoader : AssemblyLoadContext
+	{
+		// Not exactly sure about this
+		protected override Assembly Load(AssemblyName assemblyName)
+		{
+			var deps = DependencyContext.Default;
+
+			foreach (var item in deps.CompileLibraries)
+			{
+				Console.WriteLine(item.Name);
+			}
+
+			foreach (var item in deps.RuntimeLibraries)
+			{
+				Console.WriteLine(item.Name);
+			}
+
+			var res = deps.CompileLibraries.Where(d => d.Name.Contains(assemblyName.Name)).ToList();
+			var assembly = Assembly.Load(new AssemblyName(res.First().Name));
+			return assembly;
 		}
 	}
 }
